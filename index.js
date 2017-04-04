38a0c26a903994728562ddb2d8a4d46ca4908d2e
Hugo LAUSENAZ-PIRE <hugo.lausenazpire@epsi.fr>
2017-04-04 15:34:18 +0200

    Set Ship at midle

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create});
 
 function preload() {
-  game.load.spritesheet('ship', 'pictures/player.png', 32, 48);
+  game.load.spritesheet('ship', 'pictures/player.png', 42, 58);
 }
 
 function create() {
-  game.add.sprite(10, 10, 'ship');
+  game.add.sprite(400, 300, 'ship');
 }
