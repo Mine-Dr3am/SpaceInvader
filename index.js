13a470b2558a11e13cf0ad27d2a54532a953ab98
Hugo LAUSENAZ-PIRE <hugo.lausenazpire@epsi.fr>
2017-04-04 15:46:21 +0200

    TestHugo

--- index.js
+++ index.js
@@ -2,8 +2,28 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create
 
 function preload() {
   game.load.spritesheet('ship', 'pictures/player.png', 42, 58);
+  game.loas.spritesheet('invader', 'pictures/invader.png')
+
+  //Initialisation des variables
+  var tabInvader;
+  var NBR_LIGNES = 3;
+  var NBR_INVADERS_PAR_LIGNE = 7;
+
 }
 
 function create() {
   game.add.sprite(400, 300, 'ship');
+
+
+  function createInvader(nbrLignes, nbrIPLigne)
+  {
+    tabInvader = [];
+
+    for(var i; i > nbrLignes; i++)
+    {
+        tabInvader[i] 
+    }
+  }
+
+
 }
