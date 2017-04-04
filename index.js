783fa71e688a06ed5c1fc08d62a9df6376deb686
Hugo LAUSENAZ-PIRE <hugo.lausenazpire@epsi.fr>
2017-04-04 14:40:33 +0200

    try do add sprites

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
+  game.load.image('invader.png');
+  game.load.spritesheet('ship' 'pictures/player.png');
 }
 
 function create() {
+  game.add.sprite(10, 10, 'ship');
 }
 
 function update() {
