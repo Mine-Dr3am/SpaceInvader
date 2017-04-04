69fe3f71dd1d24989e44b550d319c07983a744fb
Hugo LAUSENAZ-PIRE <hugo.lausenazpire@epsi.fr>
2017-04-04 15:05:24 +0200

    add files

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create});
 
 function preload() {
-  game.load.spritesheet('ship', 'pictures/player.png', 32, 48);
+  // game.load.spritesheet('ship', 'pictures/player.png', 32, 48);
 }
 
 function create() {
-  game.add.sprite(10, 10, 'ship');
-}
-
-function update() {
+  // game.add.sprite(10, 10, 'ship');
 }
