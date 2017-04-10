0a04693f2247da7913c7adb614e36e9f4bb1f429
Hugo LAUSENAZ-PIRE <hugo.lausenazpire@epsi.fr>
2017-04-10 09:08:02 +0200

    Update(), bulletInvaders, background and comments

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create});
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
 
 //Initialisation des variables
 var aliens;
 
 function preload() {
-
+  //Import somes pictures
   game.load.spritesheet('ship', 'pictures/player.png', 32, 48);
   game.load.spritesheet('invader', 'pictures/invader32x32x4.png', 32, 32);
-
+  game.load.image('bulletInvader', 'pictures/enemy-bullet.png');
+  game.load.image('starfield', 'pictures/starfield.png');
+  game.load.image('kaboom', 'pictures/explode.png');
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  //  The baddies!
+  starfield = game.add.tileSprite(0, 0, 800, 600, 'starfield');
+
+  //  We create the aliens
     aliens = game.add.group();
     aliens.enableBody = true;
     aliens.physicsBodyType = Phaser.Physics.ARCADE;
     createInvader();
 
-  game.add.sprite(400, 300, 'ship');
+  // The enemy's bullets
+    enemyBullets = game.add.group();
+    enemyBullets.enableBody = true;
+    enemyBullets.physicsBodyType = Phaser.Physics.ARCADE;
+    enemyBullets.createMultiple(30, 'enemyBullet');
+    enemyBullets.setAll('anchor.x', 0.5);
+    enemyBullets.setAll('anchor.y', 1);
+    enemyBullets.setAll('outOfBoundsKill', true);
+    enemyBullets.setAll('checkWorldBounds', true);
+
   }
+function update(){
+  
+}
 
   function createInvader()
   //Function which create a list of invader 10 by 4 lines (v look for loop v)
@@ -33,7 +49,7 @@ function create() {
         alien.anchor.setTo(0.5, 0.5); //I don't knows :D
         alien.animations.add('fly', [ 0, 1, 2, 3 ], 20, true); // Add animation between 4 different pictures by 20 framerate
         alien.play('fly');//Display the animation wich set just before
-        alien.body.moves = false; //We can't move the aliens with controll
+        alien.body.moves = true; //We have to check this commands
       }
     }
     aliens.x = 100; // Set at x position the alien block when it's created
@@ -45,18 +61,42 @@ function create() {
     //  When the tween loops it calls descend
     tween.onLoop.add(descend, this);
   }
-  function descend() { //I don't know at this moment why we use this function
 
+  function descend() { //Aliens descend
     aliens.y += 10;
+  }
+  function setupInvader(invader)
+  {
+    invader.anchor.x = 0.5;
+    invader.anchor.y = 0.5;
+    invader.animation.add('kaboom');
 
-}
+  }function enemyFires () {
 
-  function checkDeplacement(e) {
-    if (e.keyCode == 39) {
-      shipX += 5;
-    }
-    if (e.keyCode == 37) {
-      shipX -= 4;
+      //  Grab the first bullet we can from the pool
+      enemyBullet = enemyBullets.getFirstExists(false);
 
-    }
-}
+      livingEnemies.length=0;
+
+      aliens.forEachAlive(function(alien){
+
+          // put every living enemy in an array
+          livingEnemies.push(alien);
+      });
+
+
+      if (enemyBullet && livingEnemies.length > 0)
+      {
+
+          var random=game.rnd.integerInRange(0,livingEnemies.length-1);
+
+          // randomly select one of them
+          var shooter=livingEnemies[random];
+          // And fire the bullet from this enemy
+          enemyBullet.reset(shooter.body.x, shooter.body.y);
+
+          game.physics.arcade.moveToObject(enemyBullet,player,120);
+          firingTimer = game.time.now + 2000;
+      }
+
+  }
