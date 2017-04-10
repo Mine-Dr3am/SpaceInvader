ab9313430c7f7145fe0f0d710a6575d96c1410e3
Hugo LAUSENAZ-PIRE <hugo.lausenazpire@epsi.fr>
2017-04-10 11:03:40 +0200

    Movements

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
 
 //Initialisation des variables
@@ -7,6 +6,7 @@ var firingTimer = 0;
 var livingEnemies = [];
 var Lives;
 var player;
+var cursors;
 
 function preload() {
   //Import somes pictures
@@ -15,7 +15,6 @@ function preload() {
   game.load.image('bulletInvader', 'pictures/enemy-bullet.png');
   game.load.image('starfield', 'pictures/starfield.png');
   game.load.image('kaboom', 'pictures/explode.png');
-  game.load.image('bulletPlayer', 'pictures/bullet.png');
 }
 
 function create() {
@@ -58,11 +57,26 @@ function create() {
   }
 function update(){
   starfield.tilePosition.y +=2; // scrolling background
+  cursors = game.input.keyboard.createCursorKeys();
 
-  if (game.time.now > firingTimer) //if we can shoot, we have to wait a let
+  if (game.time.now > firingTimer) //if we can shoot, we have to wait
   {
       enemyFires();
   }
+
+  if (player.alive){
+      //  Reset the player, then check for movement keys
+      player.body.velocity.setTo(0, 0);
+
+      if (cursors.left.isDown && player.x > 16){
+          player.body.velocity.x = -200;
+      }else if (cursors.right.isDown && player.x < 784){
+          player.body.velocity.x += 200;
+      }
+      cursors = game.input.keyboard.createCursorKeys();
+      fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  }
+  
 
 
 }
@@ -130,53 +144,3 @@ function update(){
           firingTimer = game.time.now + 2000;
       }
   }
-=======
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create});
-var xpos = 400;
-function preload() {
-  game.load.spritesheet('ship', 'pictures/player.png', 42, 58);
-  game.load.spritesheet('invader', 'pictures/invader.png');
-
-  //Initialisation des variables
-  var tabInvader;
-  var NBR_LIGNES = 3;
-  var NBR_INVADERS_PAR_LIGNE = 7;
-  var ship = 'pictures/player.png';
-  var player;
-  var cursors;
-
-}
-
-function create() {
-  player = game.add.sprite(400, 300, 'ship');
-  player.anchor.setTo(0.5, 0.5);
-  game.physics.enable(player, Phaser.Physics.ARCADE);
-
-  function createInvader(nbrLignes, nbrIPLigne){
-    tabInvader = [];
-
-    for(var i; i > nbrLignes; i++){
-        tabInvader[i]
-    }
-  }
-
-}
-function update() {
-    cursors = game.input.keyboard.createCursorKeys();
-    //  Scroll the background
-    starfield.tilePosition.y += 2;
-
-    if (player.alive){
-        //  Reset the player, then check for movement keys
-        player.body.velocity.setTo(0, 0);
-
-        if (cursors.left.isDown){
-            player.body.velocity.xpos = -200;
-        }else if (cursors.right.isDown){
-            player.body.velocity.xpos = 200;
-        }
-        cursors = game.input.keyboard.createCursorKeys();
-        fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-}
-}
->>>>>>> origin/master
